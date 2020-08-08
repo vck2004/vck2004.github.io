644b9048bbbaa6940b41f0975daf115f1059bcf2
vck2004 <52705258+vck2004@users.noreply.github.com>
2020-08-08 09:19:21 +0700

    Update data.js

--- blog-grid/data.js
+++ blog-grid/data.js
@@ -5,7 +5,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/26/6/8/7/c/1593164090890.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2098/5/2097476-92a67a24/128/Past%20Life%20-%20Trevor%20Daniel_%20Selena%20Gomez.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2098/6/2097476-92a67a24/128/Past%20Life%20-%20Trevor%20Daniel_%20Selena%20Gomez.mp3"
 },{
     name: "Savage love",
     genres: "Pop",
@@ -13,7 +13,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2016/01/25/4/1/1/7/1453716789458.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2095/5/2094024-573ae503/128/Savage%20Love%20Laxed%20-%20Siren%20Beat_%20-%20Jawsh.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2095/6/2094024-573ae503/128/Savage%20Love%20Laxed%20-%20Siren%20Beat_%20-%20Jawsh.mp3"
 },{
     name: "Sour candy",
     genres: "Pop",
@@ -21,7 +21,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/05/d/1/4/f/1593924618890.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2091/5/2090635-6c39c690/128/Sour%20Candy%20-%20Lady%20Gaga_%20BlackPink.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2091/6/2090635-6c39c690/128/Sour%20Candy%20-%20Lady%20Gaga_%20BlackPink.mp3"
 },{
     name: "To the moon",
     genres: "Pop",
@@ -37,7 +37,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/05/08/7/a/1/d/1588914407832.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2085/5/2084636-8d3a903f/128/Stuck%20With%20U%20-%20Ariana%20Grande_%20Justin%20Bie.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2085/6/2084636-8d3a903f/128/Stuck%20With%20U%20-%20Ariana%20Grande_%20Justin%20Bie.mp3"
 },{
     name: "Rain on me",
     genres: "Pop",
@@ -45,7 +45,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/05/22/5/a/6/8/1590121883938.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2089/5/2088697-5e7225e5/128/Rain%20On%20Me%20-%20Lady%20Gaga_%20Ariana%20Grande.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2089/6/2088697-5e7225e5/128/Rain%20On%20Me%20-%20Lady%20Gaga_%20Ariana%20Grande.mp3"
 },{
     name: "Faded",
     genres: "Pop",
@@ -69,7 +69,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/04/24/5/1/d/9/1587712339856.jpg",
-    link: "https://data25.chiasenhac.com/downloads/2083/5/2082844-b7e769fe/128/Worse%20-%20New%20Hope%20Club.mp3"
+    link: "https://data25.chiasenhac.com/downloads/2083/6/2082844-b7e769fe/128/Worse%20-%20New%20Hope%20Club.mp3"
 },{
     name: "havana",
     genres: "Pop",
@@ -85,7 +85,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/05/15/c/f/3/0/1589525866823.jpg",
-    link: "https://data18.chiasenhac.com/downloads/2012/5/2011394-1330acd7/128/Headspace%20-%20Lewis%20Capaldi.mp3"
+    link: "https://data18.chiasenhac.com/downloads/2012/6/2011394-1330acd7/128/Headspace%20-%20Lewis%20Capaldi.mp3"
 },{
     name: "Believer",
     genres: "rock",
@@ -101,7 +101,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/03/16/d/2/1/a/1584331414381.jpg",
-    link: "https://data20.chiasenhac.com/downloads/2072/5/2071835-4da3bca4/128/Brassac%20-%20Violette%20Wautier.mp3"
+    link: "https://data20.chiasenhac.com/downloads/2072/6/2071835-4da3bca4/128/Brassac%20-%20Violette%20Wautier.mp3"
 },{
     name: "No time to die",
     genres: "rock",
@@ -109,7 +109,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/02/14/a/9/d/b/1581648740327.jpg",
-    link: "https://data20.chiasenhac.com/downloads/2066/5/2065992-860081d1/128/No%20Time%20To%20Die%20-%20Billie%20Eilish.mp3"
+    link: "https://data20.chiasenhac.com/downloads/2066/6/2065992-860081d1/128/No%20Time%20To%20Die%20-%20Billie%20Eilish.mp3"
 },{
     name: "Bad guy",
     genres: "rock",
@@ -125,7 +125,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/04/23/3/9/2/6/1587628929519.jpg",
-    link: "https://data20.chiasenhac.com/downloads/2082/5/2081669-46cd39c3/128/Weird_%20-%20YUNGBLUD.mp3"
+    link: "https://data20.chiasenhac.com/downloads/2082/6/2081669-46cd39c3/128/Weird_%20-%20YUNGBLUD.mp3"
 },{
     name: "Lovely",
     genres: "rock",
@@ -157,7 +157,7 @@ let musics_data = [{
     year: "2020",
     country: "Âu Mỹ",
     img: "https://avatar-nct.nixcdn.com/song/2020/04/13/4/9/0/0/1586795435192.jpg",
-    link: "https://data19.chiasenhac.com/downloads/2034/5/2033909-6b662b47/128/Take%20What%20You%20Want%20-%20Post%20Malone_%20Ozzy%20O.mp3"
+    link: "https://data19.chiasenhac.com/downloads/2034/6/2033909-6b662b47/128/Take%20What%20You%20Want%20-%20Post%20Malone_%20Ozzy%20O.mp3"
 },{
     name: "How You Like That",
     genres: "nhạc Hàn",
@@ -165,7 +165,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/26/6/8/7/c/1593162610288.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2098/6/2097656-7b13c1ff/128/How%20You%20Like%20That%20-%20BlackPink.mp3"
 },{
     name: "Monster",
     genres: "nhạc Hàn",
@@ -173,7 +173,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/07/a/2/2/f/1594103542187.jpg",
-    link: "N/A"
+    link: "https://data25.chiasenhac.com/downloads/2101/6/2100072-2f00f605/128/Monster%20-%20Red%20Velvet%20-%20Irene%20Seulgi.mp3"
 },{
     name: "Maria",
     genres: "nhạc Hàn",
@@ -181,7 +181,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/30/1/1/b/4/1593489025151.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2099/6/2098518-a357c4ab/128/Maria%20-%20Hwasa.mp3"
 },{
     name: "Secret Story Of The Swan",
     genres: "nhạc Hàn",
@@ -189,7 +189,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/17/9/4/7/9/1592366504741.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2095/6/2094676-4f11ab43/128/Secret%20Story%20Of%20The%20Swan%20-%20IZ_ONE.mp3"
 },{
     name: "More & More",
     genres: "nhạc Hàn",
@@ -197,7 +197,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/30/1/1/b/4/1593508477563.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2092/6/2091508-70ff4639/128/More%20And%20More%20-%20TWICE.mp3"
 },{
     name: "Still With You",
     genres: "nhạc Hàn",
@@ -205,7 +205,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/05/4/f/d/2/1591338842259.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2094/6/2093520-431789b8/128/Still%20With%20You%20-%20Jungkook%20BTS_.mp3"
 },{
     name: "Mojito",
     genres: "nhạc Hoa",
@@ -213,7 +213,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/25/d/0/a/e/1593070213358.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2095/6/2094130-4ff29094/128/Mojito%20-%20Chau%20Kiet%20Luan.mp3"
 },{
     name: "Tình nhân",
     genres: "nhạc Hoa",
@@ -221,7 +221,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/05/29/a/7/1/5/1590724338812.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2090/6/2089327-0e716358/128/Tinh%20Nhan%20-%20Thai%20Tu%20Khon.mp3"
 },{
     name: "Phá Kén",
     genres: "nhạc Hoa",
@@ -229,7 +229,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/05/24/9/6/5/7/1590335883218.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2090/6/2089823-4bc14929/128/Pha%20Ken%20-%20Truong%20Thieu%20Ham.mp3"
 },{
     name: "Tớ và cậu",
     genres: "nhạc Hoa",
@@ -253,7 +253,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/01/3/8/2/7/1590993258415.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2096/6/2095329-6dd84f14/128/Xich%20Linh%20-%20Ly%20Ngoc%20Cuong.mp3"
 },{
     name: "Make You  Happy",
     genres: "nhạc Nhật",
@@ -261,7 +261,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/01/4/f/c/5/1593571629904.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2099/6/2098877-d38b313e/128/Make%20You%20Happy%20-%20NiziU.mp3"
 },{
     name: "Baby I'm A Star",
     genres: "nhạc Nhật",
@@ -269,7 +269,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/01/4/f/c/5/1593571629904.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2099/6/2098876-95f146d0/128/Baby%20I_m%20A%20Star%20-%20NiziU.mp3"
 },{
     name: " Beyond The Rainbow",
     genres: "nhạc Nhật",
@@ -277,7 +277,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/01/4/f/c/5/1593571629904.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2099/6/2098879-781f26df/128/Beyond%20The%20Rainbow%20-%20NiziU.mp3"
 },{
     name: "Lonely Universe",
     genres: "nhạc Nhật",
@@ -301,7 +301,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/03/24/0/4/4/9/1585064904183.jpg",
-    link: "N/A"
+    link: "https://data20.chiasenhac.com/downloads/2076/6/2075567-afa5f0a3/128/Haru%20wa%20Yuku%20-%20Aimer.mp3"
 },{
     name: "Last Kiss For A Person",
     genres: "nhạc Thái",
@@ -317,7 +317,7 @@ let musics_data = [{
     year: "2020",
     country: "châu á",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/12/5/b/c/b/1591944726151.jpg",
-    link: "N/A"
+    link: "https://data20.chiasenhac.com/downloads/2084/6/2083984-de4481ea/128/Forgot%20To%20Forget%20-%20Getsunova.mp3"
 },{
     name: "Tình yêu là chuyện nhỏ",
     genres: "Nhạc trẻ",
@@ -341,7 +341,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/07/a/2/2/f/1594117164476.jpg",
-    link: "N/A"
+    link: "https://data25.chiasenhac.com/downloads/2101/6/2100936-8406b356/128/That%20Tam%20Em%20Rat%20Yeu%20Anh%20-%20Dao%20Ba%20Loc.mp3"
 },{
     name: "Đợi nụ cười em",
     genres: "Nhạc trẻ",
@@ -357,7 +357,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/song/2020/07/13/a/f/7/f/1594611971401.jpg",
-    link: "N/A"
+    link: "https://data25.chiasenhac.com/downloads/2102/6/2101455-ee613ff9/128/Cu%20Chill%20Thoi%20-%20Chillies_%20Suni%20Ha%20Linh_.mp3"
 },{
     name: "Thành phố buồn",
     genres: "Trữ tình",
@@ -365,7 +365,7 @@ let musics_data = [{
     year: "1970",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/song/2017/11/30/f/e/3/c/1512014149531.jpg",
-    link: "N/A"
+    link: "https://data52.chiasenhac.com/downloads/1032/6/1031083-b2608cdf/128/Thanh%20Pho%20Buon%20-%20Truong%20Vu.mp3"
 },{
     name: "Bến cũ đò xưa",
     genres: "Trữ tình",
@@ -381,7 +381,7 @@ let musics_data = [{
     year: "N/A",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2016/01/25/4/1/1/7/1453715646175.jpg",
-    link: "N/A"
+    link: "https://data04.chiasenhac.com/downloads/1529/6/1528358-8804896e/128/Duong%20Tinh%20Doi%20Nga%20-%20Giang%20Tien_%20Duong%20N.mp3"
 },{
     name: "Trộm nhìn nhau",
     genres: "Trữ tình",
@@ -389,7 +389,7 @@ let musics_data = [{
     year: "N/A",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2018/11/28/b/a/d/9/1543393481989.jpg",
-    link: "N/A"
+    link: "https://data52.chiasenhac.com/downloads/1041/6/1040919-74fa8e7b/128/Trom%20Nhin%20Nhau%20-%20Nhu%20Quynh.mp3"
 },{
     name: "Về đâu mái tóc người thương",
     genres: "Trữ tình",
@@ -397,7 +397,7 @@ let musics_data = [{
     year: "N/A",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/3/2585.jpg",
-    link: "N/A"
+    link: "https://data51.chiasenhac.com/downloads/1010/6/1009609-4386bd27/128/Ve%20Dau%20Mai%20Toc%20Nguoi%20Thuong%20-%20Dan%20Nguyen.mp3"
 },{
     name: "Rồi mai tôi đưa em",
     genres: "Tiền chiến",
@@ -405,7 +405,7 @@ let musics_data = [{
     year: "1969",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/10/9340.jpg",
-    link: "N/A"
+    link: "https://data55.chiasenhac.com/downloads/1133/6/1132276-dad308bd/128/Roi%20Mai%20Toi%20Dua%20Em%20-%20Tung%20Duong.mp3"
 },{
     name: "Đêm đông",
     genres: "Tiền chiến",
@@ -413,7 +413,7 @@ let musics_data = [{
     year: "1939",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/10/9568.jpg",
-    link: "N/A"
+    link: "https://data55.chiasenhac.com/downloads/1136/6/1135092-8a591a52/128/Dem%20Dong%20-%20Le%20Quyen.mp3"
 },{
     name: "Cung đàn xưa",
     genres: "Tiền chiến",
@@ -421,7 +421,7 @@ let musics_data = [{
     year: "1942",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2016/01/25/4/1/1/7/1453717922492.jpg",
-    link: "N/A"
+    link: "https://data04.chiasenhac.com/downloads/1499/6/1498874-cb1cceb7/128/Cung%20Dan%20Xua%20-%20Thai%20Thanh.mp3"
 },{
     name: "Dư âm",
     genres: "Tiền chiến",
@@ -429,7 +429,7 @@ let musics_data = [{
     year: "2013",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/10/9340.jpg",
-    link: "N/A"
+    link: "https://data55.chiasenhac.com/downloads/1133/6/1132277-39e30cff/128/Du%20Am%20-%20Tung%20Duong.mp3"
 },{
     name: "Kẻ ở - Mai chị về",
     genres: "Tiền chiến",
@@ -445,7 +445,7 @@ let musics_data = [{
     year: "N/A",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2016/10/24/0/9/7/2/1477278591714.jpg",
-    link: "N/A"
+    link: "https://data51.chiasenhac.com/downloads/1003/6/1002660-5bebe558/128/Rock%20Sai%20Gon%20-%20MTV.mp3"
 },{
     name: "Người đàn bà hóa đá",
     genres: "Rock Việt",
@@ -453,7 +453,7 @@ let musics_data = [{
     year: "2002",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/4/3407.jpg",
-    link: "https://data51.chiasenhac.com/downloads/1009/5/1008290-b95e2166/128/Nguoi%20Dan%20Ba%20Hoa%20Da%20-%20Buc%20Tuong.mp3"
+    link: "https://data51.chiasenhac.com/downloads/1009/6/1008290-b95e2166/128/Nguoi%20Dan%20Ba%20Hoa%20Da%20-%20Buc%20Tuong.mp3"
 },{
     name: "À ơi",
     genres: "Rock Việt",
@@ -461,7 +461,7 @@ let musics_data = [{
     year: "2016",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/58/57696.jpg",
-    link: "https://data05.chiasenhac.com/downloads/1671/5/1670074-cd84970e/128/A%20Oi%20-%20Ngot.mp3"
+    link: "https://data05.chiasenhac.com/downloads/1671/6/1670074-cd84970e/128/A%20Oi%20-%20Ngot.mp3"
 },{
     name: "Ông trời cô đơn",
     genres: "Rock Việt",
@@ -469,7 +469,7 @@ let musics_data = [{
     year: "2011",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/27/26949.jpg",
-    link: "https://data51.chiasenhac.com/downloads/1006/5/1005573-7ddbaaf1/128/Ong%20Troi%20Co%20Don%20-%20Quai%20Vat%20Ti%20Hon.mp3"
+    link: "https://data51.chiasenhac.com/downloads/1006/6/1005573-7ddbaaf1/128/Ong%20Troi%20Co%20Don%20-%20Quai%20Vat%20Ti%20Hon.mp3"
 },{
     name: "Đám cưới chuột",
     genres: "Rock Việt",
@@ -477,7 +477,7 @@ let musics_data = [{
     year: "2011",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/1/181.jpg",
-    link: "https://data51.chiasenhac.com/downloads/1028/5/1027703-4c6ecb63/128/Dam%20Cuoi%20Chuot%20-%20Gat%20Tan%20Day.mp3"
+    link: "https://data51.chiasenhac.com/downloads/1028/6/1027703-4c6ecb63/128/Dam%20Cuoi%20Chuot%20-%20Gat%20Tan%20Day.mp3"
 },{
     name: "Tình ca Tây Bắc",
     genres: "Cách mạng",
@@ -501,7 +501,7 @@ let musics_data = [{
     year: "2002",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/20/19902.jpg",
-    link: "https://data59.chiasenhac.com/downloads/1263/5/1262682-99634698/128/Hanh%20Khuc%20Ngay%20Va%20Dem%20-%20Trong%20Tan.mp3"
+    link: "https://data59.chiasenhac.com/downloads/1263/6/1262682-99634698/128/Hanh%20Khuc%20Ngay%20Va%20Dem%20-%20Trong%20Tan.mp3"
 },{
     name: "Cuộc đời vẫn đẹp sao",
     genres: "Cách mạng",
@@ -509,7 +509,7 @@ let musics_data = [{
     year: "1971",
     country: "Vietnam",
     img: "https://data.chiasenhac.com/data/cover/1/282.jpg",
-    link: "N/A"
+    link: "https://data53.chiasenhac.com/downloads/1088/6/1087633-ee317ee4/128/Cuoc%20Doi%20Van%20Dep%20Sao%20-%20Trong%20Tan_%20Thanh.mp3"
 },{
     name: "Bài ca thống nhất",
     genres: "Cách mạng",
@@ -525,7 +525,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2019/09/12/c/3/c/7/1568253936065.jpg",
-    link: "N/A"
+    link: "https://data25.chiasenhac.com/downloads/2100/6/2099985-5615962a/128/Big%20City%20Boi%20-%20Binz_%20Touliver.mp3"
 },{
     name: "Đau vậy đủ rồi",
     genres: "Rap Việt",
@@ -541,7 +541,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/singer/avatar/2018/03/14/2/f/2/4/1521007851195.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2097/6/2096407-277c46f3/128/Tai%20Sao%20Khong%20Noi%20-%20Huynh%20James_%20Pjnboys.mp3"
 },{
     name: "Đến cuối cùng lại cô đơn",
     genres: "Rap Việt",
@@ -549,7 +549,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/16/d/b/e/b/1592283685434.jpg",
-    link: "N/A"
+    link: "https://data3.chiasenhac.com/downloads/2096/6/2095018-2d22edb9/128/Den%20Cuoi%20Cung%20Lai%20Co%20Don%20-%20Anh%20Tu%20Atus_.mp3"
 },{
     name: "Dân biển mà",
     genres: "Rap Việt",
@@ -557,7 +557,7 @@ let musics_data = [{
     year: "2020",
     country: "Vietnam",
     img: "https://avatar-nct.nixcdn.com/song/2020/06/10/b/7/1/e/1591772244006.jpg",
-    link: "N/A"
+    link: "https://data20.chiasenhac.com/downloads/2089/6/2088529-36318e6a/128/Dan%20Bien%20Ma%20-%20Huynh%20James_%20Pjnboys.mp3"
 }]
 
 let users = [{
